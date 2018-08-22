e17bc322bd09591687e047e4e5a5d323621a94c2
Ram Kesavan <ram.heptahives@gmail.com>
2018-08-22 23:24:43 +0530

    code added

--- server.js
+++ server.js
@@ -6,25 +6,20 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 app.get('/', function (req, res) {
-    // NEW CODE
     res.render('index');
 })
 
 app.get('/page', function (req, res) {
-    // NEW CODE
     res.render('page');
 })
 app.get('/anotherpage', function (req, res) {
-    // NEW CODE
     res.render('anotherpage');
 })
 
 app.get('/contact', function (req, res) {
-    // NEW CODE
     res.render('contact');
 })
 
 app.get('/example', function (req, res) {
-    // NEW CODE
     res.render('ram');
 })
